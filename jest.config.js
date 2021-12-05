9c4f64fa630fc0fa1315629cad5f628b565548a3
aamirubit <aamirsaleem167@gmail.com>
2021-12-06 00:47:15 +0500

    build: init the setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // The bail config option can be used here to have Jest stop running tests after
+    // the first failure.
+    bail: false,
+
+    // Indicates whether each individual test should be reported during the run.
+    verbose: false
+};
